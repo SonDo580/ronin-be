a0896ea1182be6c09e2edfc7ec9a63f1a3a1a161
Son Do <dominhson580@gmail.com>
2024-11-17 09:36:50 +0700

    add task

--- 15-operating-system/workspace.sh
+++ 15-operating-system/workspace.sh
@@ -17,16 +17,23 @@ open_postman() {
     postman &>/dev/null &
 }
 
-open_editor
-open_browser
-open_postman
+open_dbeaver() {
+    echo "Opening DBeaver..."
+    dbeaver-ce &>/dev/null &
+}
+
+# Run in parallel
+open_editor 
+open_browser 
+open_postman 
+open_dbeaver 
 
 echo "Workspace setup complete!"
 
 # Note:
 # &: for running the command in the background
 # &>/dev/null: redirects both standard output and standard error to /dev/null 
-#              (which discards any data sent to it)
+#              -> prevent displaying messages
 
 # Make the script executable: chmod +x workspace.sh
 # Run the script: ./workspace.sh
